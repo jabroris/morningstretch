2cc912da1940c0c95918788f7469f53dc2976136
ivailo.blagoev <ivailo.blagoev@catenate.com>
2022-02-02 21:36:59 +0100

    change to caps

--- Workout/src/com/exercises/classes/ExerciseSomeClases.java
+++ Workout/src/com/exercises/classes/ExerciseSomeClases.java
@@ -3,16 +3,18 @@ package com.exercises.classes;
 public class ExerciseSomeClases {
 	
 	public static void main(String[] args) {
-        class Tutorial {
-            public String name;
+        
 
-            public Tutorial(String tutorial) {
-                name = tutorial;
-            }
-        }
-
-        Object obj = new Tutorial("Java Quiz");
+        Object obj = new Tutorial("JAVA Quiz");
         Tutorial tutorial = (Tutorial) obj;
         System.out.println(tutorial.name);
+    }
+}
+
+class Tutorial {
+    public String name;
+
+    public Tutorial(String tutorial) {
+        name = tutorial;
     }
 }
